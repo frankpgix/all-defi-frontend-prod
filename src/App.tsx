c757a838b70402b76dccffc9c02c0819299da9d4
fungleo <web@fengcms.com>
2023-06-21 18:53:56 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import 'animate.css'
+// import 'rc-collapse/assets/index.css'
+// import 'rc-dialog/assets/index.css'
+// import 'rc-table/assets/index.css'
+//
+import React, { lazy, Suspense } from 'react'
 
+// // import { Switch, Route } from '@/components/common/Route'
+// import { Routes, Route } from 'react-router-dom'
+// import { ToastContainer } from 'react-toastify'
+// import 'react-toastify/dist/ReactToastify.css'
+//
+// import '@/style/style.scss'
+// import LoopData from '@@/common/LoopData'
+// import Footer from '@@/web/Footer'
+// import Header from '@@/web/Header'
+//
+// const Home = lazy(() => import('@/pages/Home'))
+// const Buy = lazy(() => import('@/pages/Buy'))
+// const FundMarket = lazy(() => import('@/pages/FundMarket'))
+// const MyManagement = lazy(() => import('@/pages/MyManagement'))
+// const Mining = lazy(() => import('@/pages/Mining'))
+// const Test = lazy(() => import('@/pages/Test'))
+//
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="">222</div>
+    //     <>
+    //       <div className="web-main">
+    //         <Header />
+    //         <LoopData />
+    //         <Suspense fallback={null}>
+    //           <Routes>
+    //             <Route path="/buy" element={<Buy />} />
+    //             <Route path="/fund-market/*" element={<FundMarket />} />
+    //             <Route path="/manage/*" element={<MyManagement />} />
+    //             <Route path="/all-mining" element={<Mining />} />
+    //             <Route path="/test" element={<Test />} />
+    //             <Route path="/" element={<Home />} />
+    //           </Routes>
+    //         </Suspense>
+    //       </div>
+    //       <Footer />
+    //       <ToastContainer pauseOnFocusLoss={true} pauseOnHover={true} />
+    //     </>
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+// import { WagmiConfig } from 'wagmi'
+// import { BrowserRouter } from '@/components/common/Route'
+import { BrowserRouter } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+// import { Provider } from 'react-redux'
+// import { PersistGate as _PersistGate } from 'redux-persist/integration/react'
+// import { ApolloProvider } from '@apollo/client'
+//
+// import store, { persistor } from '@/store'
+//
+// import ThemeProvider from '@/context/Theme'
+// import MobileProvider from '@/context/Mobile'
+// import WagamiClient from '@/context/WagamiClient'
+//
+// import App from '@/App'
+// import { client as ApolloClient } from '@/lib/apollo'
+// import '@/lang'
+
+// const PersistGate = _PersistGate as any
+const container = document.getElementById('all-defi')
+// @ts-ignore
+const root = createRoot(container)
+
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="">test</div>
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
+// root.render(
+//   <React.StrictMode>
+//     <MobileProvider>
+//       <ApolloProvider client={ApolloClient}>
+//         <Provider store={store}>
+//           <PersistGate loading={null} persistor={persistor}>
+//             <BrowserRouter>
+//               <WagamiClient>
+//                 <ThemeProvider>
+//                   <App />
+//                 </ThemeProvider>
+//               </WagamiClient>
+//             </BrowserRouter>
+//           </PersistGate>
+//         </Provider>
+//       </ApolloProvider>
+//     </MobileProvider>
+//   </React.StrictMode>
+// )
