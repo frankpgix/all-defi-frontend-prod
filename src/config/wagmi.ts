294b96e2de2b2168a4bc8a6cac777c886ad42e55
Frank <frank@pgix.com>
2024-01-20 11:53:10 +0800

    init

---
+++ src/config/wagmi.ts
@@ -0,0 +1,22 @@
+import { http, createConfig } from 'wagmi'
+import { mainnet, sepolia } from 'wagmi/chains'
+import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors'
+
+export const config = createConfig({
+  chains: [mainnet, sepolia],
+  connectors: [
+    injected(),
+    coinbaseWallet({ appName: 'Create Wagmi' }),
+    walletConnect({ projectId: 'b308ea2437a38bf0f2c6acf05b241f60' }),
+  ],
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+  },
+})
+
+declare module 'wagmi' {
+  interface Register {
+    config: typeof config
+  }
+}
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
+import path from 'path'
+// https://vitejs.dev/config/
+
+export default defineConfig(({ mode, command, ssrBuild }) => {
+  const root = process.cwd()
+  const env = loadEnv(mode, root)
+  console.log('env', env)
+  return {
+    define: {
+      globalThis: 'window'
+    },
+    resolve: {
+      alias: {
+        process: 'process/browser',
+        util: 'util',
+        '@': path.resolve(__dirname, 'src'),
+        '@@': path.resolve(__dirname, 'src/components')
+      }
+    },
+    plugins: [react()],
+    server: {
+      port: 5678,
+      host: '0.0.0.0'
+    }
+  }
+})
