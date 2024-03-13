b440ed2ba39cfd968929fbe3187aa45ce84143a9
Frank <frank@pgix.com>
2024-03-13 09:47:03 +0800

    init

--- src/hooks/useACProtocol.ts
+++ src/hooks/useACProtocol.ts
@@ -4,6 +4,7 @@ import { AddressType } from '@/types/base'
 import { getUnitAmount } from '@/utils/tools'
 import { getTokenByAddress } from '@/config/tokens'
 import { useNotify } from '@/hooks/useNotify'
+import { zeroAddress } from 'viem'
 
 const ACProtocolContract = useACProtocolContract()
 
@@ -20,23 +21,38 @@ export const useBuyAcToken = () => {
     const _amount = getUnitAmount(String(amount), baseToken.decimals)
     const notifyId = await createNotify({ content: 'Buy AC Token', type: 'loading' })
 
-    await writeContractAsync({
-      ...ACProtocolContract,
-      functionName: 'buy',
-      args: [baseTokenAddress, _amount],
-      account
-    })
-      .then((hash: string) => {
-        console.log(hash)
-        updateNotifyItem(notifyId, { content: 'Buy AC Token', type: 'success', hash })
+    const succNotify = (hash: string) => {
+      updateNotifyItem(notifyId, { content: 'Buy AC Token', type: 'success', hash })
+    }
+
+    const errorNotify = (msg: any) => {
+      updateNotifyItem(notifyId, {
+        title: 'Buy All Token',
+        type: 'error',
+        content: msg
       })
-      .catch((error: any) => {
-        updateNotifyItem(notifyId, {
-          title: 'Buy All Token',
-          type: 'error',
-          content: error.shortMessage
-        })
+    }
+
+    if (baseTokenAddress === zeroAddress) {
+      await writeContractAsync({
+        ...ACProtocolContract,
+        functionName: 'ethBuy',
+        args: [],
+        value: _amount,
+        account
       })
+        .then((hash: string) => succNotify(hash))
+        .catch((error: any) => errorNotify(error.shortMessage))
+    } else {
+      await writeContractAsync({
+        ...ACProtocolContract,
+        functionName: 'buy',
+        args: [baseTokenAddress, _amount],
+        account
+      })
+        .then((hash: string) => succNotify(hash))
+        .catch((error: any) => errorNotify(error.shortMessage))
+    }
   }
   return { buyAcToken }
 }
