730aa4f9abaf74d7b84931d46d5ee43ad8f208e0
Frank <frank@pgix.com>
2024-05-23 15:10:15 +0800

    update new contract address

--- src/pages/Tasks/c/MyPoints.tsx
+++ src/pages/Tasks/c/MyPoints.tsx
@@ -1,34 +1,42 @@
 import { FC } from 'react'
 
-import Popper from '@@/common/Popper'
+import Button from '@@/common/Button'
+import CopyText from '@@/common/CopyText'
 
 const MyPoints: FC = () => {
   return (
     <>
       <header className="p-task-header">My Points</header>
-      <div className="p-task-my-points">
-        <main>
-          <div className="p-task-my-points-item-total">
-            <label>
-              Total Points <Popper content="Total Points" />
-            </label>
+      <div className="p-task-my-points-layout">
+        <div className="p-task-my-points">
+          <div className="p-task-my-points-total">
+            <label>Total Points</label>
             <em>312,123</em>
+            <Button text>points rules</Button>
           </div>
           <div className="p-task-my-points-details">
             <div className="p-task-my-points-item">
-              <label>Task 2 Points</label>
+              <label>Deposit Token and Get AC Token</label>
               <em>123,123</em>
             </div>
             <div className="p-task-my-points-item">
-              <label>Task 3 Points</label>
+              <label>Stake to vault</label>
               <em>123,123</em>
             </div>
             <div className="p-task-my-points-item arrow">
               <label>Invitation Points</label>
               <em>123,123</em>
             </div>
+            <div className="p-task-my-points-item arrow">
+              <label>Number of Invitations</label>
+              <em>123,123</em>
+            </div>
           </div>
-        </main>
+          <div className="p-task-my-points-invet">
+            <p>https://alldefi.io/1e13c1e13c1e13c1e13c1e13c1e13c1e13c</p>
+            <CopyText text="https://alldefi.io/1e13c" />
+          </div>
+        </div>
       </div>
     </>
   )
