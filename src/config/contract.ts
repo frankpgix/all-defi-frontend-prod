786c87e95a1b4e055f3893a45c3fa3548f13ed6f
Frank <frank@pgix.com>
2024-09-22 19:38:16 +0800

    opt code

--- src/config/contract.ts
+++ src/config/contract.ts
@@ -7,12 +7,12 @@ import { ChainId } from '@/config'
 export const contracts: { [key: string]: AddressRec } = {
   VaultReader: {
     [ChainId.MAINNET]: zeroAddress,
-    [ChainId.BSC]: '0xd8DeB943F8e7990F25005c52d97370f5db52a19d',
+    [ChainId.BSC]: '0x3344DA0d0B9Fc428bF5aEBeA31c0fa3D0f4a62DC',
     [ChainId.BSCTEST]: '0xA0b00942671cf5af4e984EF7a98e48D0923D8575'
   },
   VaultFactory: {
     [ChainId.MAINNET]: zeroAddress,
-    [ChainId.BSC]: '0x0470c926B142DA91F6e49bca1fCF93c9999770df',
+    [ChainId.BSC]: '0x30D97A47281a9CCc03514b9180311a090e05a4d9',
     [ChainId.BSCTEST]: '0x4D0b18cD83Deb392aCAFAcb9F6174Da836433634'
   }
 }
