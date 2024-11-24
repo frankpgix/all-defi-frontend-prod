87ebf9fff136f1bd754ac50f84c957e4d4572182
Frank <frank@pgix.com>
2024-11-24 21:42:31 +0800

    init

--- src/pages/Vaults/List/c/VaultList/V2.tsx
+++ src/pages/Vaults/List/c/VaultList/V2.tsx
@@ -1,9 +1,12 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
+
+import classNames from 'classnames'
 
 import { VaultBaseInfoDefault, VaultDetailDefault } from '@/data/vault'
 import { formatNumber } from '@/utils/tools'
 import StakeButton from '@@/Vaults/StakeButton'
 import Button from '@@/common/Button'
+import Image from '@@/common/Image'
 import RoeShow from '@@/common/RoeShow'
 import { FlexBox } from '@@/core/Box'
 import Tag from '@@/core/Tag'
@@ -12,6 +15,12 @@ import { VaultName } from '@@/core/VaultName'
 // import { VaultName } from '@@/common/T'
 
 const VaultList: FC = () => {
+  const [rollStatus, setRollStatus] = useState<boolean[]>([true])
+  const onRollChange = (index: number) => {
+    const temp = [...rollStatus]
+    temp[index] = !temp[index]
+    setRollStatus(temp)
+  }
   return (
     <div className="p-vault-list">
       <header className="p-vault-list-header">
@@ -26,36 +35,62 @@ const VaultList: FC = () => {
           <strong>Active</strong>
         </header>
         <section className="table-body">
-          <dl>
-            <dt>
-              <VaultName name={"The Vault's Name"} managerName="Frank" status={0} />
-              <span>{formatNumber(12345.12233, 2, '$0,0.00')}</span>
-              <FlexBox center gap={5}>
-                <RoeShow value={11} />
-                <Tag name="Bring +++" />
-              </FlexBox>
-              <span>8</span>
-              <span>
-                <Button size="mini">Details</Button>
-              </span>
-            </dt>
-            <dd>
-              <VaultName name={"The Vault's Name"} size="mini" />
-              <span>{formatNumber(12345.12233, 2, '$0,0.00')}</span>
-              <FlexBox center gap={5}>
-                <RoeShow value={11} />
-                <Tag name="Bring +++" />
-              </FlexBox>
-              <span>8</span>
-              <span>
-                <StakeButton
-                  getData={() => null}
-                  data={VaultDetailDefault}
-                  base={VaultBaseInfoDefault}
-                />
-              </span>
-            </dd>
-          </dl>
+          {[1, 2].map((_, index) => (
+            <dl>
+              <dt>
+                <VaultName name={"The Vault's Name"} managerName="Frank" status={0} />
+                <span>{formatNumber(12345.12233, 2, '$0,0.00')}</span>
+                <FlexBox center gap={5}>
+                  <RoeShow value={11} />
+                  <Tag name="Bring +++" />
+                </FlexBox>
+                <span>8</span>
+                <FlexBox center gap={5}>
+                  <Button size="mini">Details</Button>
+                  <button
+                    className={classNames('vault-arrow', { show: rollStatus[index] })}
+                    onClick={() => onRollChange(index)}
+                  >
+                    <Image src="icon/arrow-down-blue.svg" />
+                  </button>
+                </FlexBox>
+              </dt>
+              <dd className={classNames({ show: rollStatus[index] })}>
+                <div className="vault-item">
+                  <VaultName name={"The Vault's Name"} size="mini" />
+                  <span>{formatNumber(12345.12233, 2, '$0,0.00')}</span>
+                  <FlexBox center gap={5}>
+                    <RoeShow value={11} />
+                    <Tag name="Bring +++" />
+                  </FlexBox>
+                  <span>8</span>
+                  <span>
+                    <StakeButton
+                      getData={() => null}
+                      data={VaultDetailDefault}
+                      base={VaultBaseInfoDefault}
+                    />
+                  </span>
+                </div>
+                <div className="vault-item">
+                  <VaultName name={"The Vault's Name"} size="mini" />
+                  <span>{formatNumber(12345.12233, 2, '$0,0.00')}</span>
+                  <FlexBox center gap={5}>
+                    <RoeShow value={11} />
+                    <Tag name="Bring +++" />
+                  </FlexBox>
+                  <span>8</span>
+                  <span>
+                    <StakeButton
+                      getData={() => null}
+                      data={VaultDetailDefault}
+                      base={VaultBaseInfoDefault}
+                    />
+                  </span>
+                </div>
+              </dd>
+            </dl>
+          ))}
         </section>
       </main>
     </div>
