653cc43498e5699943d694afb2b1591074da8313
Frank <frank@pgix.com>
2024-11-29 10:04:37 +0800

    fix invite page banner text error

--- src/config/contract.ts
+++ src/config/contract.ts
@@ -8,12 +8,12 @@ export const contracts: { [key: string]: AddressRec } = {
   VaultReader: {
     [ChainId.MAINNET]: '0xb4E8759042b6A8199f5df5a0E58c5bA0aFa6E3aA',
     [ChainId.BSC]: zeroAddress,
-    [ChainId.BSCTEST]: '0x4bD283EaaAc81029136088Bf9E0627C385f8e39a'
+    [ChainId.BSCTEST]: '0x1Eaf6c4c7583105Cb27242a49435ee7ec4Fc5D29'
   },
   VaultFactory: {
     [ChainId.MAINNET]: '0x9374cC6539aDDf7e5d81Ec15FBeA636B238bBe6c',
     [ChainId.BSC]: zeroAddress,
-    [ChainId.BSCTEST]: '0x96ffCcA09228250F506C6D9D29B879B3c97e3C36'
+    [ChainId.BSCTEST]: '0xDB99939a24034C345FAC6426ea11B069EF97E809'
   },
   PriceAggregator: {
     [ChainId.MAINNET]: '0x7D8d271C1D32C8615fA36E6F2B947dFe10F9A8d7',
