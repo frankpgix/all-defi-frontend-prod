214bd79be2154673ff06f02c9c0ad0b46dd758bf
Frank <frank@pgix.com>
2024-11-29 10:49:20 +0800

    fix invite page banner text error

--- src/pages/Vaults/Group/c/VaultStatus/StepLine.tsx
+++ src/pages/Vaults/Group/c/VaultStatus/StepLine.tsx
@@ -16,7 +16,7 @@ interface Props {
 const StepLine: FC<Props> = ({ data, loading }) => {
   let activeIndex = data.status
   // console.log(activeIndex)
-  // console.log(data.status, ' data.status', data)
+  console.log(data.status, ' data.status', data)
   const now = +new Date()
   if (now > data.settleEndTime && data.status === 3) {
     activeIndex = 4
@@ -33,7 +33,7 @@ const StepLine: FC<Props> = ({ data, loading }) => {
     {
       label: 'Open Period',
       popper: step1Popper,
-      time: data.epochIndex === 0 ? data.subscribeRedeemEndTime : data.epochStartTime,
+      time: data.epochIndex === 0 ? data.createTime : data.epochStartTime,
       status: 0
     },
     {
